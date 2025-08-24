583ca2bd1a05326fca1607bed7bcf45ec25e6573
Omar Eltantawy <oeltantawy8@gmail.com>
2025-08-24 23:17:40 +0300

    enhance blog page

--- src/components/blogCard/blogCard.tsx
+++ src/components/blogCard/blogCard.tsx
@@ -1,20 +1,29 @@
+import { Article } from "@/types/articles";
 import React from "react";
+import Image from "next/image";
 
-const BlogCard = () => {
+type BlogCardProps = {
+  article: Article;
+};
+
+const BlogCard = ({ article }: BlogCardProps) => {
   return (
-    <div className=" bg-white px-5 rounded-2xl shadow-md overflow-hidden hover:border-1  border-[var(--color-primary)]  hover:shadow-xl transition-all duration-500 group">
-      <img
-        src="/asets/blog-test.svg"
-        alt="Medical Blog"
-        className="w-full h-48 object-cover transition-all duration-500 group-hover:scale-110"
-      />
+    <div className="bg-white px-5 rounded-2xl shadow-md overflow-hidden border-[2px] border-white  hover:border-[var(--color-primary)] hover:shadow-xl transition-all duration-500 group">
+      <div className="relative w-full h-48 overflow-hidden">
+        <Image
+          src={article.image}
+          alt={article.title}
+          fill
+          className="object-cover transition-all duration-500 group-hover:scale-110"
+          sizes="(max-width: 768px) 100vw, 
+                 (max-width: 1200px) 50vw, 
+                 33vw"
+        />
+      </div>
 
       <div className="p-5">
-        <h4 className="font-bold text-xl mb-2">عنوان المقال</h4>
-        <p className="text-gray-600 mb-4 line-clamp-3">
-          هذا نص تجريبي لمقال طبي يوضح بعض المعلومات الصحية المفيدة للقراء. يمكن
-          أن يتضمن نصاً قصيراً لعرض المحتوى.
-        </p>
+        <h4 className="font-bold text-xl mb-2 line-clamp-1">{article.title}</h4>
+        <p className="text-gray-600 mb-4 line-clamp-2">{article.excerpt}</p>
         <a
           href="#"
           className="inline-block text-[var(--color-primary)] font-semibold hover:underline"
---
+++ src/types/articles.ts
@@ -0,0 +1,7 @@
+export type Article = {
+  id: number;
+  title: string;
+  excerpt: string;
+  body: string;
+  image: string;
+};
